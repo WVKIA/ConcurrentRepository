31a09ce2a62def651e08b8a1500a9e557a6c82be
wvkia <502332082@qq.com>
2018-04-02 21:36:44 +0800

    semaphore

---
+++ ThinkingJavaMultithread/src/Semaphore/Pool.java
@@ -0,0 +1,57 @@
+package Semaphore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+public class Pool<T> {
+    private int size;
+    private List<T> items = new ArrayList<>();
+    private volatile boolean[] checkedout;
+    private Semaphore available;    //计数信号量
+
+    public Pool(Class<T> classObject, int size) {
+
+        this.size = size;
+        checkedout = new boolean[size];
+        available = new Semaphore(size, true);
+        //加载对象池
+        for (int i = 0; i < size; i++) {
+            try {
+                items.add(classObject.newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public T checkOut() throws InterruptedException {
+        available.acquire();    //请求一个信号量
+        return getItem();
+    }
+
+    public void checkIn(T x) {
+        if (releaseItem(x)) {
+            available.release();    //回收一个信号量
+        }
+    }
+    //使用synchronized锁住共享资源
+    private synchronized T getItem() {
+        for (int i = 0; i < size; i++) {
+            if (!checkedout[i]) {
+                checkedout[i] = true;
+                return items.get(i);
+            }
+        }
+        return null;
+    }
+
+    private synchronized boolean releaseItem(T item) {
+        int index = items.indexOf(item);
+        if (index == -1) return false;
+        if (checkedout[index]) {
+            checkedout[index] = false;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ ThinkingJavaMultithread/src/Semaphore/SemaphoreDemo.java
@@ -0,0 +1,76 @@
+package Semaphore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+class CheckoutTask<T> implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private Pool<T> pool;
+
+    public CheckoutTask(Pool<T> pool) {
+        this.pool = pool;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckoutTask  " + id + " ";
+    }
+
+    @Override
+    public void run() {
+        try {
+            T item = pool.checkOut();   //迁出一个对象，代表使用
+            System.out.println(this + " check out =====" + item);
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println(this + " check in ======" + item);
+            pool.checkIn(item);
+
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
+public class SemaphoreDemo {
+    final static int SIZE=25;
+
+    public static void main(String[] args) throws InterruptedException {
+        final Pool<Fat> pool = new Pool<>(Fat.class, SIZE);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < SIZE; i++) {
+            executorService.execute(new CheckoutTask<Fat>(pool));
+        }
+        System.out.println("All checkoutTask created=============================");
+        List<Fat> list = new ArrayList<>();
+
+        for (int i = 0; i < SIZE; i++) {
+            Fat f = pool.checkOut();
+            System.out.println(i + " : main() thread checked out ======");
+            f.operation();
+            list.add(f);
+        }
+        Future<?> blocked=executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //信号量会阻塞迁出checkout操作，因为所有的信号量都被使用完了
+                    pool.checkOut();
+                } catch (InterruptedException e) {
+                    System.out.println("Checkout() Interrupted============================");
+                }
+            }
+        });
+        TimeUnit.SECONDS.sleep(2);
+        blocked.cancel(true);   //破坏阻塞
+
+        System.out.println("Checking in objects in " + list+"============");
+        for (Fat fat : list) {
+            pool.checkIn(fat);
+        }
+        for (Fat fat : list) {
+            pool.checkIn(fat);  //第二次迁入会被忽视
+        }
+        executorService.shutdown();
+    }
+}
